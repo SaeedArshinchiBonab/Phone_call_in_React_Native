0ebeff2ef9e991851cdaa5bdc39e8ea34525e1db
saeed arshinchi <arshinchi.bonab@gmail.com>
2019-05-04 14:44:22 +0430

    Font Iransans ok

--- App.js
+++ App.js
@@ -1,56 +1,58 @@
-import React, {Component} from 'react';
-import {StyleSheet, Text, View, TouchableOpacity, Linking, Platform} from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Linking, Platform } from 'react-native';
 
 export default class App extends Component {
   dialCall = () => {
- 
+
     let phoneNumber = '';
- 
+
     if (Platform.OS === 'android') {
       phoneNumber = 'tel:${+989142345131}';
     }
     else {
       phoneNumber = 'telprompt:${+989142345131}';
     }
- 
+
     Linking.openURL(phoneNumber);
   };
- 
+
   render() {
     return (
       <View style={styles.MainContainer}>
- 
+
         <TouchableOpacity onPress={this.dialCall} activeOpacity={0.7} style={styles.button} >
- 
-          <Text style={styles.TextStyle}>OPEN PHONE NUMBER IN DIAL SCREEN</Text>
- 
+
+          <Text style={styles.TextStyle}>تماس با سعید آرشین چی بناب</Text>
+
         </TouchableOpacity>
- 
+
       </View>
- 
+
     );
   }
 }
- 
+
 const styles = StyleSheet.create({
- 
+
   MainContainer: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
   },
   button: {
- 
+
     width: '80%',
     padding: 6,
     backgroundColor: '#FF6F00',
     borderRadius: 7,
   },
- 
+
   TextStyle: {
     color: '#fff',
     fontSize: 18,
     textAlign: 'center',
+    fontFamily: 'IRANSansMobile',
+    fontSize: 22
   }
- 
+
 });
